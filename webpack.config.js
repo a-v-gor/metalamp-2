3ed502f71c2f43d3bcc225263091b85637a0081a
Alexey Gorbenko <a.v.gor@mail.ru>
2024-10-03 16:41:58 +0300

    build: add pug to webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,41 @@
 const path = require('path');
+const PugPlugin = require('pug-plugin');
 
 module.exports = {
-  entry: './src/index.js',
-  output: {
-    filename: 'main.js',
-    path: path.resolve(__dirname, 'dist'),
+  plugins: [
+    new PugPlugin({
+      entry: {
+        // define many page templates here
+        index: 'src/views/index.pug', // => dist/index.html
+      },
+      js: {
+        // JS output filename
+        filename: 'js/[name].[contenthash:8].js',
+      },
+      css: {
+        // CSS output filename
+        filename: 'css/[name].[contenthash:8].css',
+      },
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.(s?css|sass)$/,
+        use: ['css-loader', 'sass-loader'],
+      },
+      {
+        test: /\.(ico|png|jp?g|webp|svg)$/,
+        type: 'asset/resource',
+        generator: {
+          filename: 'img/[name].[hash:8][ext][query]',
+        },
+      },
+    ],
   },
+  resolve: {
+    alias: {
+    "@images": path.resolve(__dirname, "./src/assets/images/"),
+   }
+ }
 };
